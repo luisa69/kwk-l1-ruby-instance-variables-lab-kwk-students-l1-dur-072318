f804b6175bec0fbabe7621f134f7d294830e5d59
Luisa Pichardo <luisapichardo@gmail.com>
2018-07-27 15:33:19 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,34 @@
 # dog.rb
+class Dog 
+  def initialize(name) 
+    @name = name 
+  end 
+  
+  def name
+    @name
+  end 
+  
+  def breed
+    @breed 
+  end 
+  
+  def color
+    @color
+  end 
+  
+  def color=(new_color)
+    @color = new_color
+  end 
+  
+  def name=(new_name)
+    @name = new_name
+  end 
+
+
+
+dog1= Dog.name ("midnight")
+puts dog1.name
+puts dog1.breed 
+puts dog1.color
+dog1.color ("jet black")
+end 
